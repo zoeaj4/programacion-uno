7e3ed498ad3179db0be83b34fc35b11dc93a0802
zoeaj4 <zoeaj4@gmail.com>
2026-03-19 10:47:10 -0300

    recursividad

--- src/practicas/practica0/SeccionSeis.java
+++ src/practicas/practica0/SeccionSeis.java
@@ -37,6 +37,28 @@ public class SeccionSeis {
 		return n * factorialRec(n-1);
 	}
 	
+	// ejercicio 31: sucesión de Fibonacci.
+	static int fibonacciRec(int n) {
+		if (n==0) return 0;
+		if (n==1) return 1;
+		return fibonacciRec(n-1) + fibonacciRec (n-2);
+	}
+	
+	static int fibonacciIter(int n) {
+	    if (n == 0) return 0;
+	    if (n == 1) return 1;
+
+	    int anterior = 0; // f(0)
+	    int actual = 1;   // f(1)
+
+	    for (int i = 2; i <= n; i++) {
+	        int siguiente = anterior + actual;
+	        anterior = actual;
+	        actual = siguiente;
+	    }
+
+	    return actual;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -45,6 +67,9 @@ public class SeccionSeis {
 		System.out.println(sumatoriaParesRec(4));
 		System.out.println(potenciaRec(4,2));
 		System.out.println(factorialRec(4));
+		// ejercicio 31
+		System.out.println(fibonacciRec(5));
+		System.out.println(fibonacciIter(5));
 	}
 
 }
