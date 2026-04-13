f4631864506f1d2300cc1e839985578dca363c6e
zoeaj4 <zoeaj4@gmail.com>
2026-04-13 19:19:07 -0300

    Práctica 1. Ejercicio 5 y 7.

---
+++ src/practicas/practica1/Persona.java
@@ -0,0 +1,60 @@
+package practicas.practica1;
+
+public class Persona {
+	
+	public Persona (String nombre, int edad) {
+		this.nombre=nombre;
+		this.edad=edad;
+	}
+	
+	boolean masJovenQue(Persona otro) {
+		if (this.edad<otro.edad) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	boolean tocayo(Persona otro) {
+		if (this.nombre.equals(otro.nombre)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	boolean mismaPersona(Persona otro) {
+		if ((this.nombre.equals(otro.nombre)) && (this.edad==otro.edad)) {
+			return true;
+		} else {
+			return false;
+		}	
+	}
+	
+	static Persona masJoven(Persona [] grupo) {
+		Persona masJoven = grupo[0];
+		for (int i = 1; i<grupo.length; i++) {
+			if (grupo[i].edad<masJoven.edad ) {
+				masJoven = grupo[i];
+				
+			}
+		}
+		return masJoven;
+	}
+	
+	String nombre;
+	int edad;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona[] grupo = new Persona[5];
+		grupo[0] = new Persona("Pablo",93);
+		grupo[1] = new Persona("Marcos",80);
+		grupo[2] = new Persona("Matias",71);
+		grupo[3] = new Persona("Juan",16);
+		grupo[4] = new Persona("Cesar",20);
+		System.out.println(masJoven(grupo).nombre);
+		}
+
+}
