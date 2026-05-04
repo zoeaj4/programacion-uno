c184db262fb71dbb3d361c3f9835fb73fdb6a08c
zoeaj4 <zoeaj4@gmail.com>
2026-05-03 22:25:33 -0300

    elmasinspeccionado

--- src/practicas/practica1/ejercicio11/Aerolinea.java
+++ src/practicas/practica1/ejercicio11/Aerolinea.java
@@ -4,7 +4,9 @@ public class Aerolinea {
     Vuelo[] vuelos;
 
 
-
+/*a) Escribir un metodo int vuelosEn(Tripulante t, String tipoAvion) que recibe un tripulante t y
+un tipo de avion e indica cuantos vuelos realizó t en aviones del tipo dado.
+ */
     int vuelosEn(Tripulante t, String tipoAvion){
         int contador = 0;
         for (int i = 0; i<vuelos.length;i++){
@@ -18,7 +20,9 @@ public class Aerolinea {
         }
         return contador;
     }
-
+/*Escribir un metodo int antiguedadPromedio(String tipoAvion) que recibe un tipo de avion y
+calcula el promedio de las antiguedades de los tripulantes en vuelos en aviones del tipo dado
+ */
     int antiguedadPromedio(String tipoAvion){
         int contadorIteraciones =0;
         int sumaAntiguedades = 0;
@@ -36,4 +40,36 @@ public class Aerolinea {
         int promedioAntiguedad=sumaAntiguedades/contadorIteraciones;
         return promedioAntiguedad;
     }
+
+    /*c) Escribir un metodo Vuelo elMasInspeccionado() que devuelve el vuelo en el que se hayan trans-
+portado mas tripulantes con cargo de “Inspector”. En caso de haber m´as de un vuelo con la
+misma cantidad de inspectores, puede devolver cualquiera de ellos
+     */
+
+    Vuelo elMasInspeccionado(){
+        Vuelo elMas = vuelos[0];
+        int maximo= 0;
+
+        for(int k = 0; k < vuelos[0].tripulacion.length;k++){
+            if(vuelos[0].tripulacion[k].cargo.equals("Inspector")){
+                maximo++;
+            }
+        }
+
+        for (int i = 1; i<vuelos.length; i++){
+            int contadorInspector1=0;
+            for (int j = 0; j<vuelos[i].tripulacion.length; j++){
+                if (vuelos[i].tripulacion[j].cargo.equals("Inspector")){
+                    contadorInspector1++;
+                }
+
+            }
+            if (contadorInspector1>maximo) {
+                elMas=vuelos[i];
+                maximo=contadorInspector1;
+            }
+        }
+        return elMas;
+    }
+
 }
